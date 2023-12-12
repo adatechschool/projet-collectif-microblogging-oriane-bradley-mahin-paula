8cbbbfb5e0a6fb9fb354872e14b7a57ede4e2ecd
Bradley-Hill <brad.michael.hill@gmail.com>
2023-12-12 15:56:01 +0000

    installed blade+breeze properly, making dashboard/profile etc look better, and added styling to home list of posts

--- resources/views/home/feed.blade.php
+++ resources/views/home/feed.blade.php
@@ -1,17 +1,17 @@
 @extends('base') 
 
-
 @section('content')
-    <h1>Feed</h1>
+    <div class="container mx-auto my-8 text-center">
+        <h1 class="text-4xl font-bold mb-6">Feed</h1>
 
-    @foreach ($posts as $post)
+        @foreach ($posts as $post)
+            <div class="bg-white p-6 mb-8 rounded-lg shadow-md border border-gray-300">
+                <h6 class="text-xl font-bold mb-2">{{ $post->user_name }}</h6>
+                <p class="text-gray-700 mb-4">{{ $post->content }}</p>
+                <p class="text-gray-500">{{ $post->created_at}}</p>
+            </div>
+        @endforeach
 
-        <div>
-            <h6>{{ $post->user_name }}</h6>
-            <p>{{ $post->content }}</p>
-            <p>{{ $post->created_at}}</p>
-        </div>
-    @endforeach
-
-    {{ $posts->links() }}  {{-- Pagination links --}}
+        {{ $posts->links() }}  {{-- Pagination links --}}
+    </div>
 @endsection
