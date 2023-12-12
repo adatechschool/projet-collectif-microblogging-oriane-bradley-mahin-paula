fd347bd020175f64f61da2ca9787ab2f3573c1ce
Orianeds <oriane.dasilva@yahoo.fr>
2023-12-12 16:56:22 +0100

    profile edition for the user connected and creation of routes to show the public and personal profile

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,36 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 >
-        {{-- class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }} --}}
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
                 </div>
+                <article>
+                    <h1>{{ $user->name }}</h1>
+                        <p>
+                            {{ $user->biography }}
+                        </p>
+                    
+                        <h2>Posts</h2>
+                        @if($user->posts->count() > 0)
+                            <ul>
+                                @foreach($user->posts as $post)
+                                    <li>
+                                        <p>{{ $post->content }}</p>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        @else
+                            <p>No posts found.</p>
+                        @endif 
+                        
+                    </article>
             </div>
         </div>
     </div>
